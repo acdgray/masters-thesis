d835b54664e1b623b4714c79142c044e9aa91857
Andrew Gray <acdgray@uwaterloo.ca>
2026-04-04 13:06:28 -0400

    Update euler2d accurcy script to include both entropy-conservative and entropy-stable scheme

--- euler2d/accuracy.py
+++ euler2d/accuracy.py
@@ -15,37 +15,57 @@ schedule = {
 
 def generate_data():
 
-    errors = {1: [], 2: [], 3: [], 4: []}
+    data = {}
+    for flag, label in [(False, "EC"), (True, "ES")]:
+        errors = {1: [], 2: [], 3: [], 4: []}
 
-    for p, Ns in schedule.items():
-        for N in Ns:
-            solution, mesh = solve(N, N, p, final_time=1, CFL=0.1, dissipation=False)
-            error = compute_error(solution, mesh)
-            errors[p].append({"h": 1 / N, "error": error})
+        for p, Ns in schedule.items():
+            for N in Ns:
+                solution, mesh = solve(
+                    N, N, p, final_time=1, CFL=0.01, dissipation=flag
+                )
+                error = compute_error(solution, mesh)
+                errors[p].append({"h": 1 / N, "error": error})
 
-    with open("data/accuracy.json", "w") as f:
-        f.write(json.dumps(errors, indent=4))
+        data[label] = errors
+
+    with open("data/euler2d_accuracy.json", "w") as f:
+        f.write(json.dumps(data, indent=4))
 
 
 def plot_data():
 
-    with open("./data/accuracy.json", "r") as f:
+    with open("./data/euler2d_accuracy.json", "r") as f:
         data = json.load(f)
+        ec_data = data["EC"]
+        es_data = data["ES"]
 
-    fig, axs = plt.subplots(1, 1, figsize=(6, 4), dpi=200)
+    fig, axs = plt.subplots(1, 2, figsize=(10, 4), dpi=200)
 
-    for p, v in data.items():
+    for p, v in ec_data.items():
         x = [d["h"] for d in v]
         y = [d["error"] for d in v]
         rate = get_least_squares_rate(x[-4:], y[-4:])
-        axs.scatter(x, y, label=f"p = {p}, rate={rate:.2f}")
-        axs.plot(x, y)
+        axs[0].scatter(x, y, label=f"p = {p}, rate={rate:.2f}")
+        axs[0].plot(x, y)
 
-    axs.set_xscale("log")
-    axs.set_yscale("log")
-    axs.set_xlabel(r"$h$")
-    axs.set_ylabel(r"$L^2$-error")
-    plt.legend()
+    for p, v in es_data.items():
+        x = [d["h"] for d in v]
+        y = [d["error"] for d in v]
+        rate = get_least_squares_rate(x[-4:], y[-4:])
+        axs[1].scatter(x, y, label=f"p = {p}, rate={rate:.2f}")
+        axs[1].plot(x, y)
+
+    axs[0].set_title("Entropy-conservative")
+    axs[1].set_title("Entropy-stable")
+
+    for i in range(2):
+        axs[i].set_xscale("log")
+        axs[i].set_yscale("log")
+        axs[i].set_xlabel(r"$h$")
+        axs[i].set_ylabel(r"$L^2$-error")
+        axs[i].legend()
+
     plt.tight_layout()
     plt.savefig("plots/euler2d_accuracy.jpg")
 
